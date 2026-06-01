f3ecbb5b08ead554e2da900b3392f129a378d32f
MidknightLN <57824755+MidknightLN@users.noreply.github.com>
2026-06-01 13:36:58 +0800

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ## dfs
+        res = []
+
+        subset = []
+        def dfs(idx:int):
+            if idx >= len(nums):
+                res.append(subset[:])
+                return
+            # 決定新增當前元素
+            subset.append(nums[idx])
+            dfs(idx+1)
+            # 決定“不要”新增當前元素
+            subset.pop()
+            dfs(idx+1)
+            ### 記得退出的時候狀態要復原 所以才會是這個順序
+        
+        dfs(0)
+        return res
+            
